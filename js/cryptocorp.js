0a2ea5c44148216a1c1b1c7ef18e33948798d703
Lior Saar <liorsaar1@gmail.com>
2014-02-09 14:18:37 -0800

    DEV-28 - sample code - indentation

--- js/cryptocorp.js
+++ js/cryptocorp.js
@@ -5,118 +5,127 @@
 var CryptoCorp = new function () {
 	
 	// configurables
-	var host = "http://btc2.hyper.to";
-    var KeyIndex = Object.freeze( { USER:0, BACKUP:1, ORACLE:2 } );
-    this.Result = Object.freeze( { SUCCESS:"success", ERROR:"error", DEFERRED:"deferred" } );
-   	
-	this.setOracleUrl = function( url ) {
-		host = url.trim();
-	};
+
+    var host = "http://btc2.hyper.to";
+    var KeyIndex = Object.freeze( {
+        USER : 0,
+        BACKUP : 1,
+        ORACLE : 2
+    } );
+    this.Result = Object.freeze( {
+        SUCCESS : "success",
+        ERROR : "error",
+        DEFERRED : "deferred"
+    } );
+
+    this.setOracleUrl = function(url) {
+        host = url.trim( );
+    }; 
 	
 	//-----------------------
 	// Create Wallet
 	//-----------------------
-	this.CreateWallet = function (data, callback) {
-        var walletId = getNewWalletId();
+    this.CreateWallet = function(data, callback) {
+        var walletId = getNewWalletId( );
         var url = getWalletUrl( walletId );
         // TODO wallet id temporary placed in the paylod, should come back in the response
-        var payload = { "walletId":url };
-        	post( url, data, callback, payload );
-	};
+        var payload = { "walletId" : url };
+        post( url, data, callback, payload );
+    }; 
 	
 	//-----------------------
 	// Get Wallet
 	//-----------------------
-	this.GetWallet = function (walletUrl, callback, payload) {
-		get( walletUrl.trim(), callback, payload );
-	};
+    this.GetWallet = function(walletUrl, callback, payload) {
+        get( walletUrl.trim( ), callback, payload );
+    }; 
 	
 	//-----------------------
 	// Sign Tx
 	//-----------------------
-	this.SignTx = function (walletUrl, data, callback, payload) {
-	    url = getWalletTxUrl( walletUrl.trim() )
-		post( url, data, callback, payload );
-	};
-	
-	// wallet url
-	function getWalletUrl(walletId) {
-		return host + "/keychains/" + walletId.trim();
-	}
-	
-	// wallet url
-	function getWalletTxUrl(walletUrl) {
-		return walletUrl.trim() + "/transactions";
-	}
-	
-	function get(url, callback, payload) {
-		$.ajax({
-			contentType: "application/json",
-			crossDomain: true,
-			url: url,
-			type: 'GET',
-			success: function ( response ) {
-				successCallback( response, callback, payload );
-			},
-			error: function( xhr, textStatus, errorThrown ) {
-				errorCallback( xhr, textStatus, errorThrown, callback, payload );
-			},
-			complete : function (xhr){
-				return;
-			}
-		});
-	}
-	
-	function post(url, data, callback, payload) {
-		var stringified = JSON.stringify( data );
-		$.ajax({
-			contentType: "application/json",
-			crossDomain: true,
-			url: url,
-			type: 'POST',
-			data: stringified,
-			success: function ( response ) {
-				successCallback( response, callback, payload );
-			},
-			error: function( xhr, textStatus, errorThrown ) {
-				errorCallback( xhr, textStatus, errorThrown, callback, payload );
-			},
-			complete : function (xhr){
-				return;
-			}
-		});
-	}
-	
+    this.SignTx = function(walletUrl, data, callback, payload) {
+        url = getWalletTxUrl( walletUrl.trim( ) )
+        post( url, data, callback, payload );
+    };
+
+    // wallet url
+    function getWalletUrl(walletId) {
+        return host + "/keychains/" + walletId.trim( );
+    }
+
+    // wallet url
+    function getWalletTxUrl(walletUrl) {
+        return walletUrl.trim( ) + "/transactions";
+    }
+
+    function get(url, callback, payload) {
+        $.ajax( {
+            contentType : "application/json",
+            crossDomain : true,
+            url : url,
+            type : 'GET',
+            success : function(response) {
+                successCallback( response, callback, payload );
+            },
+            error : function(xhr, textStatus, errorThrown) {
+                errorCallback( xhr, textStatus, errorThrown, callback, payload );
+            },
+            complete : function(xhr) {
+                return;
+            }
+        } );
+    }
+    
+    function post(url, data, callback, payload) {
+        var stringified = JSON.stringify( data );
+        $.ajax( {
+            contentType : "application/json",
+            crossDomain : true,
+            url : url,
+            type : 'POST',
+            data : stringified,
+            success : function(response) {
+                successCallback( response, callback, payload );
+            },
+            error : function(xhr, textStatus, errorThrown) {
+                errorCallback( xhr, textStatus, errorThrown, callback, payload );
+            },
+            complete : function(xhr) {
+                return;
+            }
+        } );
+    }
+    
     function getSync(url, contentType) {
         var response;
-        $.ajax({
-            contentType: contentType,
-            crossDomain: true,
-            async: false,
-            url: url,
-            success: function ( data ) {
-                response = {"result":"success", "data":data};
+        $.ajax( {
+            contentType : contentType,
+            crossDomain : true,
+            async : false,
+            url : url,
+            success : function(data) {
+                response = { "result" : "success", "data" : data };
             },
-            error: function( xhr, textStatus, errorThrown ) {
+            error : function(xhr, textStatus, errorThrown) {
                 // repackage results
                 if (xhr.readyState == 0) {
                     errorThrown = "Timeout";
                 }
-                if (response.xhr.responseText !== undefined ){
+                if (response.xhr.responseText !== undefined) {
                     errorThrown = response.xhr.responseText;
                 }
-                response = { result: "error", xhr: xhr, errorThrown: errorThrown };
+                response = { result : "error", xhr : xhr, errorThrown : errorThrown };
             },
-        });
+        } );
         return response;
     }
     
 	// post callback - success
 	function successCallback(data, callback, payload) {
 		// if malformed data - invoke callback as error
-		if( data == null  ||  data == 'undefined'  ||  data.result == 'undefined' ) {
-			var response = { result: "error", errorThrown: "No Data" };
-			postCallback( response );
+		if( data == null  ||  data === undefined  ||  data.result === undefined ) {
+			var response = { result: "error", errorThrown: "No Data", data: data };
+			callback( response, payload );
 			return;
 		}
 		// invoke callback 
@@ -124,78 +133,84 @@ var CryptoCorp = new function () {
 	}
 	
 	// post callback - ERROR
-	function errorCallback(xhr, textStatus, errorThrown, callback, payload) {
-		console.log("xhr.status:" +xhr.status);
-		console.log("xhr.responseText:"+ xhr.responseText);
-		console.log("textStatus:"+ textStatus);
-		console.log("errorThrown:" +errorThrown);
-		
-		// repackage results
-		if( xhr.readyState == 0 ) {
-		    errorThrown = "Timeout";
-		}
+    function errorCallback(xhr, textStatus, errorThrown, callback, payload) {
+        console.log( "xhr.status:" + xhr.status );
+        console.log( "xhr.responseText:" + xhr.responseText );
+        console.log( "textStatus:" + textStatus );
+        console.log( "errorThrown:" + errorThrown );
+
+        // repackage results
+        if (xhr.readyState == 0) {
+            errorThrown = "Timeout";
+        }
         // extract the error message fom the xhr
         var xhrErrorText = getXhrErrorText( xhr );
-        if( xhrErrorText != null ) {
+        if (xhrErrorText != null) {
             errorThrown += ": " + xhrErrorText;
         }
-		var response = { result: "error", xhr: xhr, errorThrown: errorThrown };
-		
-		callback( response, payload );
-	}
-	
-	function getXhrErrorText( xhr ) {
-	    // undefined
-	    if( xhr.responseText === undefined  ||  xhr.responseText == null ) {
-	        return null;
-	    }
-	    // html
-	    if( xhr.responseText.indexOf( "<html>") == 0 ) {
-            var start = xhr.responseText.indexOf("<title>")+7;
-            var end = xhr.responseText.indexOf("</title>");
-            var text = xhr.responseText.substring(start,end);
-            return text;            	        
-	    }
-	    // json
-	    try {
-            var json = JSON.parse( xhr.responseText );
-            if( json.error !== undefined ) {
-                return json.error;              
-            }
-	    } catch(e) {
-	    }
-	    // no text
-        return null;	    
-	}
-	
-	this.getWalletData = function( rulesetId, keys, parameters, pii ) {
-	
-		var data = { 
-			"rulesetId": rulesetId, 
-			"keys": keys, 
-			"parameters": parameters, 
-			"pii": pii 
-		};
-		return data;
-	};
-	
-  this.getParameters = function( value, asset, period, delay ) {
-    var parameters = { 
-      "velocity_1": { "value": parseFloat(value), "asset": asset, "period": parseInt(period), "limited_keys": [ KeyIndex.USER ] },
-      "delay_1": parseInt(delay),      
-    };
-    return parameters;
-  }
+        // callback
+        var response = { result : "error", xhr : xhr, errorThrown : errorThrown };
+        callback( response, payload );
+    }
     
-  this.getPii = function( email, first, last, phone ) {
-    // FIXME encrypt pii
-    var encrypted = "123456";
-    var pii = { "email": email, "phone": phone, "encrypted":encrypted }; // TODO all fields as members
-    return pii;
-  }
-	
-	this.getSignTxData = function( signatureIndex, bytes, inputScripts, inputTransactions, chainPaths ) {
+    function getXhrErrorText(xhr) {
+        // undefined
+        if (xhr.responseText === undefined || xhr.responseText == null) {
+            return null;
+        }
+        // html
+        if (xhr.responseText.indexOf( "<html>" ) == 0) {
+            var start = xhr.responseText.indexOf( "<title>" ) + 7;
+            var end = xhr.responseText.indexOf( "</title>" );
+            var text = xhr.responseText.substring( start, end );
+            return text;
+        }
+        // json
+        try {
+            var json = JSON.parse( xhr.responseText );
+            if (json.error !== undefined) {
+                return json.error;
+            }
+        } catch(e) {
+        }
+        // no text
+        return null;
+    }
 
+    this.getWalletData = function(rulesetId, keys, parameters, pii) {
+        return {
+            "rulesetId" : rulesetId,
+            "keys" : keys,
+            "parameters" : parameters,
+            "pii" : pii
+        };
+    };
+	
+    
+    this.getParameters = function(value, asset, period, delay) {
+        return {
+            "velocity_1" : {
+                "value" : parseFloat( value ),
+                "asset" : asset,
+                "period" : parseInt( period ),
+                "limited_keys" : [KeyIndex.USER]
+            },
+            "delay_1" : parseInt( delay ),
+        };
+    }
+    
+    this.getPii = function(email, first, last, phone) {
+        // FIXME encrypt pii
+        var encrypted = "123456";
+        var pii = {
+            "email" : email,
+            "phone" : phone,
+            "encrypted" : encrypted
+        };
+        return pii;
+    }
+	
+    this.getSignTxData = function(signatureIndex, bytes, inputScripts, inputTransactions, chainPaths) {    
         return {
             "signatureIndex" : signatureIndex,
             "transaction" : {
@@ -205,7 +220,7 @@ var CryptoCorp = new function () {
                 "chainPaths" : chainPaths
             },
         };
-	};
+    };
 	
     function pad(str, len, ch) {
         padding = '';
@@ -259,28 +274,30 @@ var CryptoCorp = new function () {
 		return redemption_script_str;
     };
     
-  function getNewWalletId() {
-    return createUUID() ;
-  }
-  
-  function createUUID() {
-    // http://www.ietf.org/rfc/rfc4122.txt
-    var s = [];
-    var hexDigits = "0123456789abcdef";
-    for (var i = 0; i < 36; i++) {
-        s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
+    function getNewWalletId() {
+        return createUUID( );
     }
-    s[14] = "4";  // bits 12-15 of the time_hi_and_version field to 0010
-    s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1);  // bits 6-7 of the clock_seq_hi_and_reserved to 01
-    s[8] = s[13] = s[18] = s[23] = "-";
-
-    var uuid = s.join("");
-    return uuid;
-  }
+    
+    function createUUID() {
+        // http://www.ietf.org/rfc/rfc4122.txt
+        var s = [];
+        var hexDigits = "0123456789abcdef";
+        for (var i = 0; i < 36; i++) {
+            s[i] = hexDigits.substr( Math.floor( Math.random( ) * 0x10 ), 1 );
+        }
+        s[14] = "4";
+        // bits 12-15 of the time_hi_and_version field to 0010
+        s[19] = hexDigits.substr( (s[19] & 0x3) | 0x8, 1 );
+        // bits 6-7 of the clock_seq_hi_and_reserved to 01
+        s[8] = s[13] = s[18] = s[23] = "-";
+    
+        var uuid = s.join( "" );
+        return uuid;
+    }
 
 
     /*
-     * get input transaxctions for script address
+     * get input transactions for script address
      */
     this.getInputTransactions = function( inputScriptString) {
         // extract the address from the script
